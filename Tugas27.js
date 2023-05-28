c7d5b8dd861b192595c2a9766cd0c9c4daa073c4
PaulNovrandes <novrandes.tanuma@gmail.com>
2023-05-28 14:04:58 +0700

    Tugas 27 JS
    Menkonversi dan menggabungkan String. Tugas ini dikumpulkan kepada niomic.

---
+++ Tugas27.js
@@ -0,0 +1,12 @@
+var a = "No ";
+var b = "rumah saya ";
+var c = 30;
+var d = "A";
+var e = "Jalan Pangeran Tirtayasa ";
+var f = 70;
+
+var hasil1 = a + b + String(c) + d;
+console.log(hasil1);
+
+var hasil2 = e + f.toString();
+console.log(hasil2);
